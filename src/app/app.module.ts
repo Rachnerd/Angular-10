161a8ecac4a67312d921e91a2127b8462057d450
Rachèl Heimbach <rachel.heimbach@rabobank.nl>
2020-10-31 20:01:40 +0100

    Chat basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChatSidebarComponent } from './chat/chat-sidebar/chat-sidebar.component';
+import { ChatMessagesComponent } from './chat/chat-messages/chat-messages.component';
+import { ChatFormComponent } from './chat/chat-form/chat-form.component';
+import { ChatChannelComponent } from './chat/chat-channel/chat-channel.component';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatSidebarComponent,
+    ChatMessagesComponent,
+    ChatFormComponent,
+    ChatChannelComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/chat/chat-sidebar/chat-sidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ov-chat-sidebar',
+  templateUrl: './chat-sidebar.component.html',
+  styleUrls: ['./chat-sidebar.component.scss']
+})
+export class ChatSidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
